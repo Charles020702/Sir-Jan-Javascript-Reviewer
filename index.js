ee2673ba0325702548b9878b7d5dc95d948a655a
andyzacal <andreazacalmorales@gmail.com>
2023-04-13 16:13:40 +0800

    First Commit

---
+++ index.js
@@ -0,0 +1,61 @@
+/*let num1 = "Charles";
+var num2 = true;
+const num3 = 3.14;
+let x = "2.54";
+let y = 2.54;
+let z = "s";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log("Hello World");
+console.log(num1, typeof num1);
+console.log(num2, typeof num2);
+console.log(num3, typeof num3);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);*/
+
+/*let x = prompt("Enter your Name:");
+console.log(x);*/
+
+/*let a = 10;
+let b = 20;
+let result = 0;
+
+result = a + b
+console.log(result);*/
+
+/*let a = 10;
+let b = 20;
+
+console.log(a == b);
+console.log(a != b);
+console.log(a < b);
+console.log(a > b);
+console.log(a >= b);
+console.log(a <= b);*/
+
+/*let a = 4;
+
+let b = 8;
+
+console.log(a < b && a != b);
+console.log(a < b && a == b);
+console.log(a < b && !(a == b));
+console.log(a < b || a != b);*/
+
+/*let a = 10;
+let b = 20;
+let c = 0;
+
+console.log(c += a * b);*/
+
+/*let x = 17/2*3+2;
+let y = 2+17/2*3;
+let b = 19 % 4 + 15 / 2 * 3;
+let a = (15 + 6) - 10 * 4;
+let z = 17 / 2 % 2 *3**3;
+ console.log(z);*/
