af0106a0444323083a0b4648db40b994f97c2496
AllanPZamora <gelgelacc1@gmail.com>
2023-05-16 18:01:30 +0800

    Tax Practice

--- index.js
+++ index.js
@@ -59,3 +59,42 @@ let b = 19 % 4 + 15 / 2 * 3;
 let a = (15 + 6) - 10 * 4;
 let z = 17 / 2 % 2 *3**3;
  console.log(z);*/
+
+/*var Fname = "Charles Valdez"; 
+var age = 21;
+
+console.log("My name is " , Fname, "and I'm", age,"Years Old.");*/
+
+/*let stars = "********************************"
+
+console.log(stars);
+console.log("* Programming Assignment 1     *");
+console.log("* Computer Progamming 2        *");
+console.log("* Author: Duffy Ducky          *");
+console.log("* Due Date: Thursday, Jan. 23  *");
+console.log(stars);*/
+
+
+
+ document.getElementById("btnsubmit").onclick =  function calculate(){
+    let name = document.getElementById("txtname").value;
+    let gross = document.getElementById("txtgross").value;
+    
+    document.getElementById("txtnamed").value = name;
+    document.getElementById("txtgrossed").value = gross.toLocaleString(undefined, 2);
+
+    let stateTax = 0.035 * gross;
+    let socialSecurity = 0.0575 * gross;
+    let pagibig = 0.0275 * gross;
+    let philhealth = 200.0;
+    let retirementPlan = 0.05 * gross;
+    let netpay = gross - stateTax - socialSecurity - pagibig - philhealth - retirementPlan;
+
+    document.getElementById("txtst").value = stateTax.toLocaleString(undefined, 2);
+    document.getElementById("txtssc").value = socialSecurity.toLocaleString(undefined, 2);
+    document.getElementById("txtpic").value = pagibig.toLocaleString(undefined, 2);
+    document.getElementById("txtphc").value = philhealth.toLocaleString(undefined, 2);
+    document.getElementById("txtrp").value = retirementPlan.toLocaleString(undefined, 2);
+    document.getElementById("txtnp").value = netpay.toLocaleString(undefined, 2);
+
+}
